6c297f178ec5a3e6539d03583b0978be743a1406
dglabella <dglabella@gmail.com>
2021-12-14 11:30:17 -0300

    saving progress

--- src/components/CellularAutomaton.jsx
+++ src/components/CellularAutomaton.jsx
@@ -6,55 +6,52 @@ import PlayArrowRoundedIcon from "@mui/icons-material/PlayArrowRounded";
 const gridSquareSize = 10;
 
 const CellularAutomaton = (props) => {
-    let [isRunning, setIsRunning] = useState(props.running);
-    let [execOutputData, setExecOutputData] = useState(props.execOutputData);
-    let [generationIndex, setGenerationIndex] = useState(0);
-    const columnsQuantity = execOutputData.params.colsQuantity;
+	let [isRunning, setIsRunning] = useState(props.running);
+	let [execOutputData, setExecOutputData] = useState(props.execOutputData);
+	let [generationIndex, setGenerationIndex] = useState(0);
+	const columnsQuantity = execOutputData.params.colsQuantity;
 
-    console.log("cols", columnsQuantity);
+	console.log("cols", columnsQuantity);
 
-    const run = useCallback(() => {
-        if (!isRunning) {
-            return;
-        }
+	const run = useCallback(() => {
+		if (!isRunning) {
+			return;
+		}
 
-        console.log("isRunning");
+		console.log("isRunning");
 
-        setGenerationIndex(++generationIndex);
+		setGenerationIndex(++generationIndex);
 
-        if (generationIndex === execOutputData.generations.lenght) {
-            setIsRunning((isRunning = false));
-        }
-        setTimeout(run, 500);
-    }, []);
+		if (generationIndex === execOutputData.generations.length - 1) {
+			setIsRunning((isRunning = false));
+		}
+		setTimeout(run, 500);
+	}, []);
 
-    return (
-        <>
-            <IconButton
-                aria-label="play"
-                onClick={() => {
-                    run();
-                }}
-            >
-                <PlayArrowRoundedIcon />
-            </IconButton>
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns: `repeat(${columnsQuantity},${gridSquareSize}px)`
-                }}
-            >
-                {execOutputData.generations[generationIndex].map((row, i) =>
-                    row.map((cellState, j) => (
-                        <GameOfLifeCell
-                            key={`(${i},${j})`}
-                            cellState={cellState}
-                        />
-                    ))
-                )}
-            </div>
-        </>
-    );
+	return (
+		<>
+			<IconButton
+				aria-label="play"
+				onClick={() => {
+					run();
+				}}
+			>
+				<PlayArrowRoundedIcon />
+			</IconButton>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: `repeat(${columnsQuantity},${gridSquareSize}px)`
+				}}
+			>
+				{execOutputData.generations[generationIndex].map((row, i) =>
+					row.map((cellState, j) => (
+						<GameOfLifeCell key={`(${i},${j})`} cellState={cellState} />
+					))
+				)}
+			</div>
+		</>
+	);
 };
 
 export default CellularAutomaton;
